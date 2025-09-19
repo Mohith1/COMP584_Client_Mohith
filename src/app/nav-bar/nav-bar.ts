512317fd74a029f472e6a9863cb47f2b82c7a594
Mohith Kanthamneni <mohith81999@gmail.com>
2025-09-19 16:29:31 -0700

    Beautification using Angular Materials

--- src/app/nav-bar/nav-bar.ts
+++ src/app/nav-bar/nav-bar.ts
@@ -1,10 +1,18 @@
+import { M } from '@angular/cdk/keycodes';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from "@angular/router";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterLink],
+  imports: [RouterLink, 
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.scss'
 })
